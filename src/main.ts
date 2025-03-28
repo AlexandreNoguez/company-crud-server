c60df65eb92c87fd157aa5899e39de1c8931d0ec
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2025-03-28 19:08:43 -0300

    chore: add swagger and initial config to

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  const isDev = configService.get<string>('NODE_ENV') !== 'production';
+
+  if (isDev) {
+    const config = new DocumentBuilder()
+      .setTitle('KPMG API')
+      .setDescription('API for managing companies')
+      .setVersion('1.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
+    console.log('Swagger running at /api');
+  }
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
