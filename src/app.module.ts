b0bd0870ffbaa6bf6be62106796e137d88599ae2
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2025-03-28 16:11:00 -0300

    feat: add basic company resource module and shared basic email service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CompanyModule } from './modules/company/company.module';
+import { ConfigModule } from '@nestjs/config';
+import { EmailService } from './shared/email/email.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +(process.env.DB_PORT ?? 5432),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true, // use false in production, should be used only in development
+      logging: process.env.DB_LOGGING === 'true',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      migrations: ['dist/migrations/*{.ts,.js}'],
+    }),
+    CompanyModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, EmailService],
 })
 export class AppModule {}
