491f710935459d6f9b164eec2d06a085e915e4ee
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2025-03-29 02:22:23 -0300

    fix(email-template) created constants and enum to template/titles

---
+++ src/constants/email-templates.ts
@@ -0,0 +1,10 @@
+import { EmailTemplatesNames } from '../enums/email-templates.enum';
+
+export const COMPANY_CREATED_TEMPLATE: EmailTemplatesNames =
+  EmailTemplatesNames.COMPANY_CREATED_TEMPLATE;
+
+export const COMPANY_UPDATED_TEMPLATE: EmailTemplatesNames =
+  EmailTemplatesNames.COMPANY_UPDATED_TEMPLATE;
+
+export const NEW_COMPANY_CREATED_TITLE = 'Nova empresa cadastrada!';
+export const COMPANY_UPDATED_TITLE = 'Empresa atualizada com sucesso!';
---
+++ src/enums/email-templates.enum.ts
@@ -0,0 +1,5 @@
+// email-templates.enum.ts
+export enum EmailTemplatesNames {
+  COMPANY_CREATED_TEMPLATE = 'company-created.hbs',
+  COMPANY_UPDATED_TEMPLATE = 'company-updated.hbs',
+}
