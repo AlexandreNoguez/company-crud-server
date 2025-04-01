eb8701d3ace1a7fa0d87ef8747ac864604004c55
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2025-04-01 00:40:03 -0300

    chore(unused-files) remove unused files

--- src/shared/helpers/error.helper.ts
+++
@@ -1,4 +0,0 @@
-export function formatError(contextMessage: string, error: unknown): Error {
-  const message = error instanceof Error ? error.message : 'Unknown error';
-  return new Error(`${contextMessage}: ${message}`);
-}
